babbe9dbb06c5482be6e0da1fe875c10d815cefb
Aizar <aizar.cabrera@icloud.com>
2018-10-09 17:27:34 -0600

    adds pseudo code for some methods and repl

--- lib/board.rb
+++ lib/board.rb
@@ -1,25 +1,17 @@
 class Board
 
-
-  def y_coord
-    y_coord = [@row_1] #@row_2, @row_3]
-  end
-
+  # def y_coord
+  #   y_coord = [@row_1] #@row_2, @row_3]
+  # end
+  #
   def row_1
-    @row_1{
-      1 => "x"
-    }
+    @row_1 = {"A" => "x"}
   end
 
 end
 
 
-
-
-
-
-
-# board = Board.new
-# p board.row_3.values# START WITH ROW 1 ON THE BOTTOM FOR X,Y EASE
+board = Board.new
+p board.row_1.values# START WITH ROW 1 ON THE BOTTOM FOR X,Y EASE
 # p board.row_2.values
 # p board.row_1.values
--- test/board_test.rb
+++ test/board_test.rb
@@ -2,5 +2,15 @@ require './test/test_helper'
 
 class BoardTest < Minitest::Test
 
+def test_it_exists
+  board = Board.new
+  assert_instance_of Board, board
+end
+
+def test_it_can_have_a_coordinate
+  board = Board.new
+
+  assert_equal  , board.y_coord
+end
 
 end

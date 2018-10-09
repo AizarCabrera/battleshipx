6e2f8bc223b60ed150ac05903e43b7cf457a7629
Aizar <aizar.cabrera@icloud.com>
2018-10-09 14:55:35 -0600

    add tests for space

--- lib/board.rb
+++ lib/board.rb
@@ -2,12 +2,12 @@ require 'pry'
 
 class Board
 
-  def initialize
-    @display = [['. ', '1 ', '2 ', '3 ', '4 '],
-                ['A'] + Array.new(4),
-                ['B'] + Array.new(4),
-                ['C'] + Array.new(4),
-                ['D'] + Array.new(4)
-              ]
-  end
+  # def initialize
+  #   @display = [['. ', '1 ', '2 ', '3 ', '4 '],
+  #               ['A'] + Array.new(4),
+  #               ['B'] + Array.new(4),
+  #               ['C'] + Array.new(4),
+  #               ['D'] + Array.new(4)
+  #               ]
+  # end
 end
--- test/space_test.rb
+++ test/space_test.rb
@@ -14,6 +14,6 @@ class SpaceTest < Minitest::Test
     assert_equal ({}), @space.coordinates
   end
 
-  
+
 
 end

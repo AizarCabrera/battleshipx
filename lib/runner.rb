c38fc78997f2f3123d33ff6e2d922c4cf5782f9d
Nico <nicovigil1@gmail.com>
2018-10-10 15:26:32 -0600

    adds the computer ship object (coordinate & health)

--- lib/runner.rb
+++ lib/runner.rb
@@ -2,11 +2,23 @@ require './place_ship'
 require './board'
 
 
-ship1 = Place_ship.new(2)
-ship2 = Place_ship.new(3)
+# ship1 = Place_ship.new(2)
+# ship2 = Place_ship.new(3)
+#
+# p ship1
+# p ship2
 
-p ship1
-p ship2
+
+### SHOWS THAT NEW SHIPS CAN BE PLACED. 
+computer_destroyer = Ship.new(2)
+computer_submarine = Ship.new(3)
+
+c_destroyer_place = PlaceShip.new(computer_destroyer.health)
+c_submarine_place = PlaceShip.new(computer_submarine.health)
+
+p c_destroyer_place
+p c_submarine_place
+
 
 
 board = Board.new

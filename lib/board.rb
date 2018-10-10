36c15a357043bee990079d299e77ed3a577ec6aa
Nico <nicovigil1@gmail.com>
2018-10-10 09:01:23 -0600

    forgot to add board file to last commit. (adds board & ability to pass coordinates)

--- lib/board.rb
+++ lib/board.rb
@@ -1,25 +1,58 @@
-class Board
+class Board # still needs an initialize
+  # attr_reader :row_1, :row_2, :row_3
+  # attr_reader :display_board
+  def initialize
+  end
 
+  def coord(x, y)
+    coord[y-1] = [@row_1[x], @row_2[x], @row_3[x], @row_4[x]]
+  end
 
   def y_coord
-    y_coord = [@row_1] #@row_2, @row_3]
+    # row_1 = {1:"x", 2:"x", 3:"x"}
+    # row_2 = {1:"x", 2:"x", 3:"x"}
+    # row_3 = {1:"x", 2:"x", 3:"x"}
   end
 
-  def row_1
-    @row_1{
-      1 => "x"
-    }
-  end
 
+    def row_1
+      @row_1 = { #  x coordinates
+        1 => [ ],
+        2 => [ ],
+        3 => [ ],
+        4 => [ ]
+      }
+    end
+
+    def row_2
+      @row_2 = {
+        1 => [ ],
+        2 => [ ],
+        3 => [ ],
+        4 => [ ]
+      }
+    end
+
+    def row_3
+      @row_3 = {
+        1 => [ ],
+        2 => [ ],
+        3 => [ ],
+        4 => [ ]
+      }
+    end
+
+    def row_4
+      @row_4 = {
+        1 => [ ],
+        2 => [ ],
+        3 => [ ],
+        4 => [ ]
+      }
+    end
+
+    # def display_board <= ## ASK ABOUT HOW TO MAKE A METHOD PRINT THEM ALL OUT
+    #   display_hashes = [row_1.values, row_2.values, row_3.values, row_4.values]
+    #
+    # end
 end
-
-
-
-
-
-
-
-# board = Board.new
-# p board.row_3.values# START WITH ROW 1 ON THE BOTTOM FOR X,Y EASE
-# p board.row_2.values
-# p board.row_1.values

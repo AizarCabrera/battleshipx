63ca78d347429069749dc271b025a65eb9de195c
Aizar <aizar.cabrera@icloud.com>
2018-10-11 18:14:48 -0600

    adds method enter coordinates and shot to game class

--- lib/game.rb
+++ lib/game.rb
@@ -12,23 +12,23 @@ class Game
   puts 'I have laid out my ships on the grid. You now need to layout your two ships.'
   puts 'The first is two units long and the second is three units long.'
   puts 'The grid has A1 at the top left and D4 at the bottom right.'
-  input = gets.chomp
+  enter_coordinates
   end
 
 
-  def enter_coordinates
+  def self.enter_coordinates
     puts 'Enter the squares for the two-unit ship:'
     input = gets.chomp
-    @board.validate_coordinates
+    # @board.validate_coordinates
     # if coordinates are valid, the user is prompt to take a shot
     shot
   end
 
-    def shot
-      input = gets.chomp
-      # the user takes a shot
-      print board
-    end
+  def self.shot
+    input = gets.chomp
+    # the user takes a shot
+    print board
+  end
 
 
 

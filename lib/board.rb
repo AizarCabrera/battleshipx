95266aa7ee880c7c454fb3989f33c6ebbc4af6d3
Nico <nicovigil1@gmail.com>
2018-10-13 17:55:26 -0600

    adds indididual cells to board

--- lib/board.rb
+++ lib/board.rb
@@ -1,19 +1,21 @@
+require './lib/cell'
+require './lib/space'
+
 class Board
   attr_reader :row_num,
               :row_a,
               :row_b,
               :row_c,
-              :row_d
-
+              :row_d,
+              :cells
   def initialize
+    @cells = Space.new.populate_cells
     @row_num = { 1 => ['0'], 2 => ['1'], 3 => ['2'], 4 => ['3'], 5 => ['4'] }
-    @row_a = { 1 => ['A'], 2 => [], 3 => [], 4 => [], 5 => [] }
-    @row_b = { 1 => ['B'], 2 => [], 3 => [], 4 => [], 5 => [] }
-    @row_c = { 1 => ['C'], 2 => [], 3 => [], 4 => [], 5 => [] }
-    @row_d = { 1 => ['D'], 2 => [], 3 => [], 4 => [], 5 => [] }
+    @row_a = { 1 => ['A'], 2 => [@cells['A1'].display], 3 => [@cells['A2'].display], 4 => [@cells['A3'].display], 5 => [@cells['A4'].display] }
+    @row_b = { 1 => ['B'], 2 => [@cells['B1'].display], 3 => [@cells['B2'].display], 4 => [@cells['B3'].display], 5 => [@cells['B4'].display] }
+    @row_c = { 1 => ['C'], 2 => [@cells['C1'].display], 3 => [@cells['C2'].display], 4 => [@cells['C3'].display], 5 => [@cells['C4'].display] }
+    @row_d = { 1 => ['D'], 2 => [@cells['D1'].display], 3 => [@cells['D2'].display], 4 => [@cells['D3'].display], 5 => [@cells['D4'].display] }
   end
-
-  def populate_board_with_cells; end
 
   def display_board
     p row_num.values
@@ -23,3 +25,6 @@ class Board
     p row_d.values
   end
 end
+
+board = Board.new
+p board.display_board

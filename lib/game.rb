0c8b10cb401ae277d7e73eed55578cb304321098
Aizar <aizar.cabrera@icloud.com>
2018-10-13 22:46:39 -0600

    removes class place ship not used

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,17 @@
 require './lib/board'
 require './lib/ship'
 require './lib/space'
+require './lib/player'
 require 'pry'
 
 class Game
   attr_reader :guess
   def self.begin
-    @guess = nil
+    # @guess = nil
     @board    = Board.new
     @computer = Player.new('computer')
     @human    = Player.new('human')
-    # computer do this method of placing ships randomly (@compouter.place_random)
+    # computer do this method of placing ships randomly (@computer.place_random)
     display
     puts 'I have laid out my ships on the grid. You now need to layout your two ships.'
     puts 'The first is two units long and the second is three units long.'
@@ -25,10 +26,10 @@ class Game
   def self.enter_coordinates
     puts 'Enter the squares for the two-unit ship:'
     input = gets.chomp
-    @guess = Space.new
-    @guess.place_user_ships(input.upcase)
-    @guess.store_value
-    p @board.display_board
+    # @guess = Space.new
+    # @guess.place_user_ships(input.upcase)
+    # @guess.store_value
+    # p @board.display_board
 
     # @board.validate_coordinates
     # if coordinates are valid, the user is prompt to enter coordinates for a three unit ship
